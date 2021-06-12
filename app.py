7f91de3e4ae75d01c0168258f3abf6dac44b1989
Gaurav V <gv71.msrit@gmail.com>
2021-06-12 18:21:24 +0530

    Finished basic ui

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/c')
+def character():
+    return render_template('character.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
