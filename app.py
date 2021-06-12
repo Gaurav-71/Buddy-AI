df11de5487ca817a670e454ad2ff2e2c30bade53
Gaurav V <gv71.msrit@gmail.com>
2021-06-12 20:37:06 +0530

    Added message queueing system

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
 from flask import Flask, render_template, request, redirect
+import time
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+class Messages():
+    def __init__(self, sender, message, mtype):
+        self.sender = sender
+        self.message = message
+        self.mtype = mtype
+        
 
-@app.route('/c')
+messagesQ = []
+
+messagesQ.insert(0,Messages("user","Hi from user","str"))
+messagesQ.insert(0,Messages("bot",["Hi from bot","2nd","3rd"], "list"))
+
+
+@app.route('/',methods=['POST','GET'])
+def index():
+    return render_template('index.html', messages = messagesQ)
+
+@app.route('/chats',methods=['POST','GET'])
 def character():
-    return render_template('character.html')
+    if request.method == 'POST':
+        user_query = request.form['query']
+        messagesQ.insert(0,Messages("user",user_query,"str"))
+        time.sleep(0)
+        messagesQ.insert(0,Messages("bot","analyzed reply from bot","str"))
+        return redirect('/')
+    else:
+        return render_template('index.html', messages = messagesQ)
 
 if __name__ == "__main__":
     app.run(debug=True)
